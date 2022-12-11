18ce0b3a7a93a8f395adb6dfbf20f2760286d6cc
Guillermo Lòpez Garcìa <willy@MacBook-Pro-de-Guillermo.local>
2022-12-10 23:23:38 -0600

    deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://guillermo907.github.io',
+  base: '/astrotest'
+});
